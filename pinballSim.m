ca5e78a0bdf15e0c04233e6b734ab971a77654d6
Sung <iyw03211@gmail.com>
2015-11-30 11:34:20 -0500

    Add x dimension to model flat topview surface of pinball floor

--- pinballSim.m
+++ pinballSim.m
@@ -6,26 +6,30 @@ function res = pinballSim()
     g = 9.8;
     
     function res = sim(~,P)
-        y = P(1); %y pos
-        vy = P(2); %y velocity
+        x = P(1); %x pos
+        vx = P(2); %x velocity
+        y = P(3); %y pos
+        vy = P(4); %y velocity
         
         yForce = -g*sind(fieldSlope); %Force in y direction
-        disp(yForce)
+        %disp(yForce)
         
+        dxdt = 0;
+        dvxdt = 0;
         dydt = vy;
         dvydt = yForce/m;
         
-        res = [dydt; dvydt];
+        res = [dxdt; dvxdt; dydt; dvydt];
         
     end
 
     options = odeset('Events',@events);
     function [value, isterminal, direction] = events (t, X)
-        value = X(1);
+        value = X(3);
         isterminal = 1;
         direction = -1;
     end
 
-    [T, R] = ode45(@sim, [0, 10], [initYPos, initYVelo],options);
-    plot(T, R(:,1));
+    [T, R] = ode45(@sim, [0, 10], [0, 0, initYPos, initYVelo],options);
+    plot(R(:,1), R(:,3));
 end
