2a23e384995b5abe697aa761bb4d9fa1b033752c
Sung <iyw03211@gmail.com>
2015-12-02 20:09:41 -0500

    Add simulation script and x dimension parameter

--- pinballSim.m
+++ pinballSim.m
@@ -1,8 +1,10 @@
-function res = pinballSim()
+function res = pinballSim(xvel)
     fieldSlope = 6.5; %in degrees
     m = 0.08; %kg, mass of pinball
+    initXPos = 0;
+    initXVelo = xvel;
     initYPos = 0;
-    initYVelo = 20; %m/s
+    initYVelo = 2; %m/s
     g = 9.8;
     
     function res = sim(~,P)
@@ -12,9 +14,10 @@ function res = pinballSim()
         vy = P(4); %y velocity
         
         yForce = -g*sind(fieldSlope); %Force in y direction
+        disp(yForce)
         %disp(yForce)
         
-        dxdt = 0;
+        dxdt = vx;
         dvxdt = 0;
         dydt = vy;
         dvydt = yForce/m;
@@ -30,12 +33,12 @@ function res = pinballSim()
         direction = -1;
     end
 
-    [T, R] = ode45(@sim, [0:0.1:10], [0, 0, initYPos, initYVelo],options);
+    [T, R] = ode45(@sim, [0:0.01:10], [initXPos, initXVelo, initYPos, initYVelo],options);
     
     X = R(:,1); %a sequence of x position of pinball
     Y = R(:,3); %y position
     
-    minmax = [min(X)-10, max(X)+10, min(Y)-10, max(y)+20];
+    minmax = [min(X)-1, max(X)+1, min(Y), max(y)+0.5];
     
     for i=1:length(T)
         clf;
@@ -47,7 +50,7 @@ function res = pinballSim()
     end
     
     function draw_func(x, y)
-        plot(x, y, 'r.', 'MarkerSize', 20);
+        plot(x, y,'r.', 'MarkerSize', 20);
     end
     
 end
